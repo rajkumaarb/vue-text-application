a6d966c152546a08d19db32919a62d2a519c06fe
Bharath <psbharathrajkumaar@agilysys.com>
2020-08-13 17:37:07 +0530

    Modified additional test cases

--- tests/unit/FileContentDisplay.spec.js
+++ tests/unit/FileContentDisplay.spec.js
@@ -1,6 +1,5 @@
 import FileContentDisplay from '@/components/FileContentDisplay.vue'
 import { shallowMount } from '@vue/test-utils'
-import sinon from 'sinon'
 
 describe ('File Content Display', () => {
 
@@ -34,7 +33,7 @@ describe ('File Content Display', () => {
     expect(contentComponent.exists()).toBe(true);
   })
 
-  it('DOM is rendered', () => {
+  it('wrapper component is available for user', () => {
     expect(contentComponent.classes()).toContain('my-wrapper')
   })
 
@@ -44,11 +43,22 @@ describe ('File Content Display', () => {
     expect(contentComponent.vm._data.displaydata).toBe(contentComponent.vm._data.toData)
   })
 
-  xit('search filed should be cleared when clicking clear button', async () => {
+  it('search button is clicked', () => {
+    contentComponent.vm.doSearch = jest.fn()
+    contentComponent.find('input').trigger('keyup.enter')
+    expect(contentComponent.vm.doSearch).toHaveBeenCalled()
+  })
+
+  it('clear button is clicked', () => {
     contentComponent.vm._data.search.text = 'spec'
-    contentComponent.vm._clearData = sinon.stub()
+    contentComponent.vm._data.search.count = 1
+    contentComponent.vm.clearData = jest.fn(() => {
+      contentComponent.vm._data.search.text = '';
+      contentComponent.vm._data.search.count = 0;
+    })
     contentComponent.findAll('button').at(0).trigger('click')
-    await contentComponent.vm.$nextTick()
+    expect(contentComponent.vm.clearData).toHaveBeenCalled()
     expect(contentComponent.vm._data.search.text).toBe('')
+    expect(contentComponent.vm._data.search.count).toBe(0)
   })
 })
